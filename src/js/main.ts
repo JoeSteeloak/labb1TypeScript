b7850dc8b139bac474a07178106492a0c49044f5
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-03-21 14:26:54 +0100

    button fix

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,6 +1,28 @@
+/* Interface för en kurs */
 interface CourseInfo {
     code: string;
     name: string;
     progression: string;
     syllabus: string;
 }
+
+/* variabler */
+const saveBtnEl = document.getElementById('saveBtn') as HTMLButtonElement;
+
+/* Event listener */
+saveBtnEl.addEventListener('click', saveCourse, false);
+
+/* funktion för att spara en kurs */
+function saveCourse(): void {
+
+    /* Lagra kursvariablerna */
+    const codeInput: string = (document.getElementById("code") as HTMLInputElement).value;
+    const nameInput: string = (document.getElementById("name") as HTMLInputElement).value;
+    const progressionInput: string = (document.querySelector('input[name="progression"]:checked') as HTMLInputElement).value;
+    const syllabusInput: string = (document.getElementById("syllabus") as HTMLInputElement).value;
+
+    console.log(codeInput, nameInput, progressionInput, syllabusInput);
+
+
+}
+
