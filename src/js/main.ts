23c55c928469d17eac232f17aabda952db6a8b0a
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-03-22 07:57:28 +0100

    add local storage (working)

--- src/js/main.ts
+++ src/js/main.ts
@@ -9,9 +9,11 @@ interface courseInfo {
 /* variabler */
 const saveBtnEl = document.getElementById('saveBtn') as HTMLButtonElement;
 const courseListEl = document.getElementById('courseList') as HTMLDivElement;
+const clearAllBtnEl = document.getElementById('clearAllBtn') as HTMLButtonElement;
 
 /* Event listener */
 saveBtnEl.addEventListener('click', saveCourse, false);
+clearAllBtnEl.addEventListener('click', clearAllCourses, false);
 
 /* funktion för att spara en kurs */
 function saveCourse(): void {
@@ -35,15 +37,31 @@ function saveCourse(): void {
 
     /* skriv ut kursen till DOM */
     courseListEl.innerHTML += `
+    <div class='course'>
     <ul>
     <li>Kurskod: ${newCourse.code}</li>
     <li>Kursnamn: ${newCourse.name}</li>
     <li>Progression: ${newCourse.progression}</li>
     <li><a href=${newCourse.syllabus}>Länk till kursplanen</a></li>
     </ul>
+    </div>
     `;
+
+    /* rensa formuläret */
+    (document.getElementById("courseForm") as HTMLFormElement).reset() ;
+
+    /* spara till local storage */
+    saveToLocalStorage(newCourse);
 
 }
 
 /* Rensa alla kurser */
+function clearAllCourses(): void {
+courseListEl.innerHTML = '';
+}
 
+/* funktion för att spara till local storage */
+function saveToLocalStorage(item : courseInfo): void {
+    
+localStorage.setItem('item', JSON.stringify(item));
+}
